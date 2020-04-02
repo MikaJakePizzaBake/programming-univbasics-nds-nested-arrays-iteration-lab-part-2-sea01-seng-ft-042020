e62c81fe416da43378b137b8603c8936f9831ead
Trevor Low <tlow94@uw.edu>
2020-04-02 22:25:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,9 +1,12 @@
 
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-  def find_greater_pair(src)
-      array.push(src[counter].max)
-      counter +=1
-    end
-    array
+def find_greater_pair(src)
+  array = []
+  counter = 0
+  while counter < src.length
+    array.push(src[counter].max)
+    counter += 1
   end
+  array
+end
